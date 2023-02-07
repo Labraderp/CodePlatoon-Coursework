350a47b8ea9d100d0de1c56cfd1e226afe7d08f0
Labraderp <kalithesharpclaw@gmail.com>
2023-02-06 20:35:45 -0500

    Added challenge problem from deaf_grandma.js

--- Week_1/Day_1/deaf_grandma/deaf_grandma.js
+++ Week_1/Day_1/deaf_grandma/deaf_grandma.js
@@ -9,46 +9,51 @@
 */
 
 function textCheck(input) {
+    //Checks for no input
     if(input === '') {
         return 'blank';
     }
-
+    //Checks for "GOODBYE!"
     if(input === 'GOODBYE!') {
         return 'bye'
     }
-
+    //Checks for ALL CAPS INPUT that isn't GOODBYE!
     if(input === input.toUpperCase()) {
         return 'upper';
     }
-
-    //SPEAK UP
+    //Assumes lowercase/not a word final answer
     return 'lower';
 }
 
 function deaf_gma(input) {
-    let flag = textCheck(input);
+    let flag = textCheck(input);        //'blank', 'bye', 'upper', 'lower'
+    
     switch(flag) {
+        //flag === blank
         case 'blank':
             let blank_resp = prompt("WHAT?!");
             deaf_gma(blank_resp);
             break;
+        
+        //flag === bye
         case 'bye':
-            if(input === "GOODBYE!") {                      //if GOODBYE!
-                let bye_resp = prompt("LEAVING SO SOON?");      //Save prompt
-                if(bye_resp === "GOODBYE!") {                   //If second GOODBYE!
+            if(input === "GOODBYE!") {                      
+                let bye_resp = prompt("LEAVING SO SOON?");
+                //If second prompt === "GOODBYE!", posts alert without user input.
+                if(bye_resp === "GOODBYE!") {                  
                     alert("LATER, SKATER!");
-                    break;                                  //You're done
-                } else {                                    //otherwise
-                    deaf_gma(bye_resp);                         //start again
+                    break;                                 
+                } else {
+                    deaf_gma(bye_resp);
                     break;
                 }
             }
-        
+        //flag === lower
         case 'lower':
             let lower_resp = prompt("SPEAK UP, KID!");
             deaf_gma(lower_resp);
             break;
-
+        //flag === upper
         case 'upper':
             let upper_resp = prompt("NO, NOT SINCE 1946!");
             deaf_gma(upper_resp);
@@ -56,9 +61,60 @@ function deaf_gma(input) {
     }
 }
 
+function lowercase_check(input) {
+    console.log(`Lowercase check came back as ${/[a-z]/.test(input)}`);
+    return /[a-z]/.test(input);
+    
+}
+
+function regex_grandma(input) {
+    //Check for blank input
+    if(input === '') {
+        let resp = prompt("WHAT?!");
+        return regex_grandma(resp);
+    }
+    //Check for GOODBYE!
+    if(input === 'GOODBYE!') {
+        let resp = prompt("LEAVING SO SOON?");
+        //Conditionals for potential responses in the nested prompt
+        switch(resp) {
+            case 'GOODBYE!':                        //Check for GOODBYE!
+                return alert("LATER, SKATER!");
+            case '':                                //Check for blank
+                resp = prompt("WHAT?!");
+                return regex_grandma(resp);
+            default:
+                if(lowercase_check(resp) === true) {
+                    resp = prompt("SPEAK UP!");
+                    return regex_grandma(resp);
+                } else {
+                    resp = prompt("NO, NOT SINCE 1946!");
+                    return regex_grandma(resp);
+                }
+        }
+    }
+    //Check for lowercase characters
+    if(lowercase_check(input)) {
+        let resp = prompt("SPEAK UP, KID!");
+        return regex_grandma(resp);
+    }
+    //Default to UPPERCASE but not GOODBYE!
+    let resp = prompt("NO, NOT SINCE 1946!");
+    return regex_grandma(resp);
+}
+
+function main() {
+    let input = prompt("HEY KID!");
+    regex_grandma(input);
+}
+
+main();
+
+/**
 function main() {
     let input = prompt("HEY KID!");
     deaf_gma(input);
 }
 
 main();
+**/
