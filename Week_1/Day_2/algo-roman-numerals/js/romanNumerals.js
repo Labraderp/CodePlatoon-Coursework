8e48a0bcd04ef0357bd9b46844c0fe9ef3b09502
Labraderp <kalithesharpclaw@gmail.com>
2023-02-07 18:15:36 -0500

    formatting

--- Week_1/Day_2/algo-roman-numerals/js/romanNumerals.js
+++ Week_1/Day_2/algo-roman-numerals/js/romanNumerals.js
@@ -2,11 +2,13 @@ exports.toRoman = function(num) {
         let numerals = {'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,'L':50,
                         'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1};
         roman_str = '';
+
         for(val in numerals) {
           while(num >= numerals[val]) {
             roman_str += val;
             num -= numerals[val];
           }
         }
+        
         return roman_str;
 }
