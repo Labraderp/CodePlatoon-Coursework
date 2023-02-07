8735570ac9897259cbc20f901a580fd57e5b4f3f
Labraderp <kalithesharpclaw@gmail.com>
2023-02-07 15:57:44 -0500

    completed fibonacci problem set

--- Week_1/Day_2/algo-fibonacci/js/fibonacci.js
+++ Week_1/Day_2/algo-fibonacci/js/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (num) => {
-    
+    let fib_arr = [0, 1];
+
+    if(num === 0 || num === 1) {
+        return fib_arr[num];
+    } else {
+        let counter = 2;
+        while(counter <= num) {
+            let last_index = fib_arr.length-1;
+            fib_arr.push(fib_arr[last_index]+fib_arr[last_index-1]);
+            counter++;
+        }
+        return fib_arr[fib_arr.length-1];
+    }
 }
 
 module.exports = {fibonacci}
