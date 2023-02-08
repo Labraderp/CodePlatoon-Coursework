3a63e29f86f2319d36cef2558bccfda79429fa43
Labraderp <kalithesharpclaw@gmail.com>
2023-02-08 15:23:40 -0500

    code cleaned up

--- Week_1/Day_3/deaf_grandma.py
+++ Week_1/Day_3/deaf_grandma.py
@@ -21,10 +21,9 @@ def deaf_grandma(resp):
             deaf_grandma(resp)
 
     #Lowercase
-    else:
-        resp = input("SPEAK UP, KID!")
-        deaf_grandma(resp)
-        return
+    resp = input("SPEAK UP, KID!")
+    deaf_grandma(resp)
+    return
 
 resp = input("HEY KID!" )
 deaf_grandma(resp)
