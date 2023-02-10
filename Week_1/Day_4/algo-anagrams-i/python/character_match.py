86cc437cb6cb6c1a09e601ab9dda4ea7c88c0595
Labraderp <kalithesharpclaw@gmail.com>
2023-02-10 09:48:02 -0500

    assignments completed further

--- Week_1/Day_4/algo-anagrams-i/python/character_match.py
+++ Week_1/Day_4/algo-anagrams-i/python/character_match.py
@@ -2,8 +2,8 @@
 
 # Part 1
 def is_character_match(string1, string2):
-    string1 = string1.lower()
-    string2 = string2.lower()
+    string1 = string1.lower().replace(' ', '')
+    string2 = string2.lower().replace(' ', '')
     letter_dict1 = {}
     letter_dict2 = {}
     
@@ -21,16 +21,20 @@ def is_character_match(string1, string2):
         else:
             letter_dict2.update({letter:1})
         
-    for key in letter_dict1:
-        if key in letter_dict2:
-            if letter_dict1[key] == letter_dict2[key]:
-                continue
-            else:
-                return False
-        else:
-            return False
- 
-    return True
+    # for key in letter_dict1:
+    #     if key in letter_dict2:
+    #         if letter_dict1[key] == letter_dict2[key]:
+    #             continue
+    #         else:
+    #             return False
+    #     else:
+    #         return False
+    # return True
+    print(letter_dict1)
+    print(letter_dict2)
+    if letter_dict1 == letter_dict2:
+        return True
+    return False
     
 
 # Part 2
@@ -74,12 +78,12 @@ def anagrams_for(word, list_of_words):
 
 list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds", "pplea"]
 
-print(anagrams_for("threads", list_of_words)) #== ["threads", "trashed", "hardest", "hatreds"])
-print(anagrams_for("apple", list_of_words)) #== [])
+#print(anagrams_for("threads", list_of_words)) #== ["threads", "trashed", "hardest", "hatreds"])
+#print(anagrams_for("apple", list_of_words)) #== [])
 
 
-# print(is_character_match('Anna Madrigal', 'A man and a girl'))
-# print(is_character_match('charm', 'march'))
-# print(is_character_match('zach', 'attack'))
-# print(is_character_match('CharM', 'mARcH'))
-# print(is_character_match('abcde2', 'c2abed'))
+print(is_character_match('Anna Madrigal', 'A man and a girl')) 
+print(is_character_match('charm', 'march'))                     
+print(is_character_match('zach', 'attack'))                     
+print(is_character_match('CharM', 'mARcH'))
+print(is_character_match('abcde2', 'c2abed'))
--- Week_1/Day_4/algo-armstrong-numbers/python/armstrong_numbers.py
+++ Week_1/Day_4/algo-armstrong-numbers/python/armstrong_numbers.py
@@ -1,14 +1,34 @@
-results = []
-for i in range(len(arr)):
-    string_num = str(arr[i])
-    n = len(string_num)
-    answer = 0
-    for num in range(len(string_num)):
-        new_num = int(string_num[num])
-        answer += new_num ** n
-    if answer == arr[i]:
-        results.append(answer)
+def armstrong_number(arr):
 
+    results = []
+
+    for i in range(len(arr)):
+        string_num = str(arr[i])
+        n = len(string_num)
+        answer = 0
+
+        for num in range(len(string_num)):
+            new_num = int(string_num[num])
+            answer += new_num ** n
+        
+        if answer == arr[i]:
+            results.append(answer)
+    
     return results
 
+def new_arm(arr):
+    results = []
+
+    for num in arr:
+        total = 0
+        for i in str(num):
+            total += int(i) ** len(str(num))
+        
+        if total == int(num):
+            results.append(total)
+
+    return results
+        
+
 print(armstrong_number((list(range(0,999)))))
+print(new_arm((list(range(0,999)))))
