2d546b133c86b51be5d0a69cdeb67929e1600944
Labraderp <kalithesharpclaw@gmail.com>
2023-02-13 13:21:20 -0500

    completed oop-app-users assignment

--- Week_2/Day_1/oop-app-users-i/User.py
+++ Week_2/Day_1/oop-app-users-i/User.py
@@ -1,1 +1,14 @@
 # your User class goes here
+
+class User():
+    def __init__(self, fname, lname, DL_num):
+        self.fname = fname
+        self.lname = lname
+        self.email = f'{fname}.{lname}@labraderp.dog'
+        self.DL_num = DL_num
+
+john = User('John', 'Denver', 902100904)
+saul = User('Saul', 'Goodman', 8675309)
+
+print(john.fname)
+print(saul.DL_num)
