4499fdd42997aa6baa401688adbe00541b1ff155
Labraderp <kalithesharpclaw@gmail.com>
2023-03-02 18:14:53 -0500

    finished guessing game

--- Week_4/Day_4/html-number-guessing-game/app.js
+++ Week_4/Day_4/html-number-guessing-game/app.js
@@ -1,2 +1,43 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+const input = document.getElementById('userinput');
+const button = document.getElementById('submitbutton');
+const guess_area = document.getElementById('guessbox');
+
+let answer = rngen();
+console.log(answer);
+
+button.addEventListener('click', e => {
+    if (!isNaN(input.value)) {
+        validate_answer(input.value);
+    } else {
+        console.log("Must use integer values only!");
+    }
+    input.value = "";
+})
+
+function validate_answer(input) {
+    if(input === '') {
+        console.log("Can't use an empty submission!");
+        return;
+    } else {
+        const node = document.createElement("li");
+        node.append(input);
+
+        if(input == answer) {
+            alert("YOU WIN!");
+            return;
+        } else if(input > answer) {
+            alert("Input is too high!");
+            guess_area.appendChild(node);
+            console.log(guess_area);
+        } else {
+            alert("Input is too low!");
+            guess_area.appendChild(node);
+            console.log(guess_area);
+        } 
+    }
+}
+
+function rngen() {
+    number = Math.floor((Math.random() * 100)+1);
+    return number;
+}
