1a4e90741e5249def62ebbfee77a2c7f2d40224d
Labraderp <kalithesharpclaw@gmail.com>
2023-03-16 14:40:49 -0400

    Pushing previous day's work

--- Week_6/Day_3/django-associations/imdb/moviedb/models.py
+++ Week_6/Day_3/django-associations/imdb/moviedb/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    role_title = models.CharField(max_length=50)
 
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=50)
+
+class Actor(models.Model):
+    actor_name = models.CharField(max_length=50)
+    movies = models.ManyToOneRel(Movie)
+    role = models.OneToOneField(Role)
